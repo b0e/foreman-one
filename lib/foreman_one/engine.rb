b1b290aad6a2efa459eb9e0832cdc5dda81a8182
Achim Ledermüller <achim.ledermueller@netways.de>
2017-02-15 16:52:22 +0100

    removed unnecessary require

--- lib/foreman_one/engine.rb
+++ lib/foreman_one/engine.rb
@@ -25,7 +25,6 @@ module ForemanOne
     config.to_prepare do
       begin
         # extend fog opennebula server/
-        require 'fog/one/models/compute/server'
         require File.expand_path('../../../app/models/concerns/fog_extensions/one/server', __FILE__)
 
         Fog::Compute::One::Server.send(:include, ::FogExtensions::One::Server)
